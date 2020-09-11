83c245b6f65cdf69db2576af5d6fdba73a371b65
AnandaCipta <ciptaananda67@gmal.com>
2020-09-11 09:23:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jumlahbilangan/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.jumlahbilangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText bill1;
+    private EditText bill2;
+    private EditText jumlah;
+    private Button btnhasil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUI();
+        initEvent();
+    }
+
+    private void initUI() {
+        bill1 = (EditText) findViewById(R.id.txtbill);
+        bill2 = (EditText) findViewById(R.id.txtbill2);
+        jumlah = (EditText) findViewById(R.id.jumlah);
+        btnhasil = (Button) findViewById(R.id.btnhitung);
+    }
+    private void initEvent() {
+        btnhasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hitungJumlah();
+            }
+        });
+    }
+
+    private void hitungJumlah() {
+        int angka1 = Integer.parseInt(bill1.getText().toString());
+        int angka2 = Integer.parseInt(bill2.getText().toString());
+        int total = angka1 + angka2;
+        jumlah.setText(total+"");
+    }
+}
